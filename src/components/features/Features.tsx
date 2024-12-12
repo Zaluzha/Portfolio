6134b284e6738dc449636fe9e0e39965bac27e83
zaluzha <hey.aku.zaky@gmail.com>
2024-12-12 09:47:17 +0700

    fifth commit

--- src/components/features/Features.tsx
+++ src/components/features/Features.tsx
@@ -10,7 +10,7 @@ const Features = () => {
       <Title title="Features" des="What I Do" />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         {featuresData.map((item) => (
-          <Card item={item} key={item.id} />
+          <Card {...item} key={item.id} />
         ))}
       </div>
     </section>
